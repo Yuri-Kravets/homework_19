08373daa7da32f9b0a51efb8f024f6f528b3c538
Yuri-Kravets <kravets.yuri.v@gmail.com>
2021-11-01 14:08:51 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Task 1
+/*let arr = ['a','b','c','d'];
+
+alert(arr[0] + '+' + arr[1] + ',' + arr[2] + '+' + arr[3]);
+*/
+// Task 2
+/*let arr = [2,5,3,9];
+
+let result = ((arr[0]*arr[1])+(arr[2]*arr[3]));
+console.log(result);
+*/
+// Task 3
+
+/*let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+
+  alert(arr[1][0]);
+*/
+// Task 4
+/*let obj = {js:['jQuery', 'Angular'], php:'hello', css:'world'};
+  alert(obj.js[0]);
+  */
+ // Task 5
+ let arr = [];
+  for (i = 0; i <= 10; i++) {
+    let newArr = x += x
+    arr.push(x);
+  }
+  console.log(arr);
+
