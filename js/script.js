b016192342d9d7c9c4925a8707bf19ff60b979cd
Yuri-Kravets <kravets.yuri.v@gmail.com>
2021-11-01 14:59:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,10 +20,100 @@ console.log(result);
   alert(obj.js[0]);
   */
  // Task 5
- let arr = [];
-  for (i = 0; i <= 10; i++) {
-    let newArr = x += x
-    arr.push(x);
-  }
-  console.log(arr);
+ var arr = [];
+ var x = 'x';
+ var i = 1;
+ do {
+ arr.push(x);
+ i++;
+ x += 'x';
+ } while (i <= 10);
 
+console.log(arr);
+
+
+var arr = [];
+var str = '';
+for (var i=0; i < 10; i++) {
+str += 'x';
+arr.push(str);
+}
+console.log(arr);
+  
+// task 6
+/*var arr = [];
+var str;
+for (var i=1; i<10; i++) {
+str = '';
+for (var j=1; j<=i; j++) {
+str += i;
+}
+arr.push(str);
+}
+console.log(arr);
+
+
+///////////////////
+Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var sum = 0;
+for (var i=0; i<arr.length; i++) {
+sum += arr[i];
+if (sum > 10) {
+break;
+}
+}
+console.log(i+1);
+Дан массив с числами. Не используя метода reverse переверните его элементы в обратном порядке.
+function arr(a) {
+var b = [];
+for (var i=a.length-1; i>=0; i--) {
+b.push(a[i]);
+}
+return b;
+}
+console.log(arr([1, 5, 6, 7]));
+Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+function sum(a) {
+var b=0;
+for (var i=0; i<a.length; i++) {
+for (var j=0; j<a[i].length; j++){
+b += a[i][j];
+}
+}
+return b;
+}
+console.log(sum([[1, 2, 3], [4, 5], [6]]));
+Дан трехмерный массив с числами, например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+
+function sum(a) {
+var b=0;
+for (var i=0; i<a.length; i++) {
+for (var j=0; j<a[i].length; j++){
+for (var k=0; k<a[i][j].length; k++){
+b += a[i][j][k];
+}
+}
+}
+return b;
+}
+console.log(sum([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]));
+
+
+
+
+
+
+
+// task 11
+/*var arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+	for (var j = 0; j < arr[i].length; j++) {
+  		for (var k = 0; k < arr[i][j].length; k++) {
+			sum += arr[i][j][k];
+   		}
+	}
+}
+alert(sum);
+*/
