dafabcf5a86f7f9b6dc537b3b76871bfec02c8d8
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-11-02 13:55:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,41 +20,65 @@ console.log(result);
   alert(obj.js[0]);
   */
  // Task 5
- var arr = [];
- var x = 'x';
- var i = 1;
- do {
- arr.push(x);
- i++;
- x += 'x';
- } while (i <= 10);
-
-console.log(arr);
-
-
-var arr = [];
-var str = '';
-for (var i=0; i < 10; i++) {
-str += 'x';
-arr.push(str);
+/*let arr = [];
+let str = '';
+	for (let i=0; i < 10; i++) {
+		str += 'x';
+		arr.push(str);
 }
 console.log(arr);
-  
+ */ 
 // task 6
-/*var arr = [];
-var str;
-for (var i=1; i<10; i++) {
-str = '';
-for (var j=1; j<=i; j++) {
-str += i;
+/*let arr = [];
+let str;
+for (let i = 1; i < 10; i++) {
+	str = '';
+	for (let a = 1; a <= i; a++) {
+		str += i;
+	}
+	arr.push(str);
 }
-arr.push(str);
+console.log(arr);*/
+// Task 7 
+/*function arrayFill(value, lenght) {
+  let arr =  Array(lenght).fill(value);
+  console.log(arr);
 }
-console.log(arr);
+arrayFill('x', 8);
+*/
+// Task 8
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let sum = 0;
+	for (let i = 0; i < arr.length; i++){
+		sum += arr[i];
+		if (sum > 10) {
+			break;
+		}
+		console.log(i+1);
+	}*/
+// Task 9
+/*let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function reverse(a) {
+	let b = [];
+	for (let i = a.length-1; i >= 0; i--) {
+	b.push(a[i]);
+	}
+	return b;
+	}
+	console.log(reverse(array));
+	*/
+	// Task 10
+let arr = [[1, 2, 3], [4, 5], [6]];
+let arr2 = arr.flat();
+console.log(arr2); 
 
-
+	let result = arr2.reduce(function(sum, elem) {
+		return sum + elem;
+	}, 0);
+	
+	console.log(result);
 ///////////////////
-Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+/*Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var sum = 0;
 for (var i=0; i<arr.length; i++) {
